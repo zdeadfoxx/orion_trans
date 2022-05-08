713d2849fb5415a3246b9ce0e0a78a67b8a3e855
zoo <zdeadfoxx@bk.ru>
2022-05-08 23:53:12 +0700

    min js

---
+++ scripts/application.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector('.burger__img')
+const head__menu = document.querySelector('.header__menu');
+const callBack_btn = document.querySelector('.form__btn');
+
+if(burger){
+    burger.addEventListener('click', function(e) 
+    {
+    burger.classList.toggle("_active");
+    head__menu.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+
+});
+}
+
+const item = document.querySelectorAll('.item__menu');
+
+if(item.length >0){
+    item.forEach(item =>{
+        item.addEventListener("click", function (e) {
+            const item = e.target;
+            if(burger.classList.contains('_active')){
+                burger.classList.remove("_active");
+                head__menu.classList.remove('_active');
+                document.body.classList.remove('_lock');
+            }
+            window.scrollTo({
+                behavior: "smooth"
+            })
+        });
+    });
+}
