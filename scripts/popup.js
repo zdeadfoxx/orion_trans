5453fbc06607800a3ca66c11654db95c191c491f
zoo <zdeadfoxx@bk.ru>
2022-06-14 19:49:58 +0700

    fix_form

--- scripts/popup.js
+++ scripts/popup.js
@@ -30,10 +30,58 @@ if(item.length >0){
     });
 }
 
-if(callBack_btn){
-    callBack_btn.addEventListener('click',CallBack);
+if (callBack_btn) {
+    callBack_btn.addEventListener('click', CallBack);
 }
 function CallBack() {
     const info = document.querySelector('.info__text-callback');
     info.textContent = info.textContent == 'Данные отправлены' ? 'Данные отправлены' : 'Данные отправлены';
 }
+
+
+
+
+
+let form;
+let action;
+function findElements() {
+  form = document.querySelector('form');
+  ({ action } = form);
+}
+function showMessage(data) {
+  alert(data.message);
+}
+function onSuccess(response) {
+  return response.json().then(showMessage);
+}
+function onError(data) {
+  console.error(data);
+}
+function collectData(currentForm) {
+  const data = new FormData(currentForm);
+  return data;
+}
+function setOptions(currentForm) {
+  return {
+    method: 'post',
+    body: collectData(currentForm),
+  };
+}
+function sendForm(currentForm) {
+  return fetch(action, setOptions(currentForm));
+}
+function onSubmit(event) {
+  event.preventDefault();
+  const { currentTarget } = event;
+  sendForm(currentTarget)
+    .then(response => onSuccess(response, currentTarget))
+    .catch(onError);
+}
+function subscribe() {
+  form.addEventListener('submit', onSubmit);
+}
+function init() {
+  findElements();
+  subscribe();
+}
+init();
